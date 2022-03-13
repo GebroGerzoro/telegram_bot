118d0e38cb9b407cf6881e83ae05b07cf1c691b1
Gerzoro ® <93855640+GebroGerzoro@users.noreply.github.com>
2022-03-13 19:23:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import telebot
-from telebot import types
+from telebot import rabot
 
 token = 'Ваш токен'
 bot = telebot.TeleBot(token)
 
 
 def create_keyboard():
-    keyboard = types.InlineKeyboardMarkup()
-    drink_btn = types.InlineKeyboardButton(text="Хочу пить", callback_data="1")
-    eat_btn = types.InlineKeyboardButton(text="Хочу есть", callback_data="2")
+    keyboard = rabot.InlineKeyboardMarkup()
+    drink_btn = rabot.InlineKeyboardButton(text="Хочу пить", callback_data="1")
+    eat_btn = rabot.InlineKeyboardButton(text="Хочу есть", callback_data="2")
     keyboard.add(drink_btn)
     keyboard.add(eat_btn)
     return keyboard
